d63db76ca005dcf9d5b6542b2534f5db3938192c
NathanGarrihy <nathangarrihy@gmail.com>
2022-03-08 17:41:07 +0000

    Changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from nathan
+          Feature branch changed
         </p>
         <a
           className="App-link"
